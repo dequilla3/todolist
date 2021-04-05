01228b40112b4b0f2fd60abbb847b418d423c382
Kim Dequilla <67781364+dequilla3@users.noreply.github.com>
2021-04-04 18:25:59 -1000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import React, { useState } from "react";
+//importing components
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+        <h1>Kim's Todo List</h1>
+      </header>
+      <Form setInputText={setInputText} />
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
